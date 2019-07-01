3dac99210551e91674e12a928a444a8dfc249db2
Yuri <yuri@rawbw.com>
2019-07-01 00:45:58 -0700

    Fix process uid. Check that the process runs as root.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
 
 #include "args.h"
 #include "spec.h"
+#include "util.h"
 #include "commands.h"
 
+#include <rang.hpp>
+
+#include <unistd.h>
+
+#include <iostream>
+
 int main(int argc, char** argv) {
+
+  //
+  // can only run as at privileged level because we need to run chroot(8) and need to create jails
+  //
+  if (geteuid() != 0) {
+    std::cerr << rang::fg::red << "crate has to run as root user" << rang::style::reset << std::endl;
+    return 1;
+  }
+
+  //
+  // adjust uid, make it equal to euid
+  //
+  Util::ckSyscallError(setuid(geteuid()), "setuid", "geteuid()");
 
   //
   // parse the arguments
