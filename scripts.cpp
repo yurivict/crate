e53d04819b945a81da1aa332d415ff6df9fb1444
Yuri <yuri@rawbw.com>
2019-07-13 01:05:47 -0700

    Print the header/footer messages before and after the script is run.

--- scripts.cpp
+++ scripts.cpp
@@ -1,8 +1,11 @@
 #include "scripts.h"
 #include "util.h"
 
+#include <rang.hpp>
+
 #include <string>
 #include <map>
+#include <iostream>
 #include <ostream>
 
 namespace Scripts {
@@ -31,8 +34,11 @@ static std::string escape(const std::string &script) {
 void section(const char *sec, const std::map<std::string, std::map<std::string, std::string>> &scripts, FnRunner fnRunner) {
   auto it = scripts.find(sec);
   if (it != scripts.end())
-    for (auto &script : it->second)
+    for (auto &script : it->second) {
+      std::cout << rang::fg::cyan << "@run-script#" << sec << "#" << script.first << "#begin" << rang::style::reset << std::endl;
       fnRunner(STR("/bin/sh -c \"" << escape(script.second) << "\""));
+      std::cout << rang::fg::cyan << "@run-script#" << sec << "#" << script.first << "#end" << rang::style::reset << std::endl;
+    }
 }
 
 }
